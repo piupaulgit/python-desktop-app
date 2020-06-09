98cdeba2e8e9a2d3f502d298cc7c1b90a373ca63
piupaulgit <contact.piu619@gmail.com>
2020-06-09 11:37:19 +0530

    transparent window added

--- chat-app/public/electron.js
+++ chat-app/public/electron.js
@@ -9,7 +9,12 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({
+    width: 900, 
+    height: 680,
+    frame: false,
+    transparent: true
+  });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
 }
